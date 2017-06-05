536180d1f396ca5a0c7d42bb29b1f9dbf1f74a66
Jormangandur <=>
2017-06-05 17:30:38 +0100

    new/edit item page style

--- app/mod_catalog/helpers.py
+++ app/mod_catalog/helpers.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import abort
+from flask import render_template
 from app import db_session
 from app import login_manager
 from flask_login import login_required, login_user, logout_user, current_user
@@ -22,11 +23,9 @@ def check_item_creator(item):
 
 
 def render_new_item_page(title="", description="", category_id=None):
-    categories = db_session.query(Category).all()
     # category_id used when editing not creating new item
     if category_id is not None:
         category_id = int(category_id)
     return render_template('new_edit_item.html.j2', page_heading='Create Item',
-                           categories=categories,
                            title=title, description=description,
                            category_id=category_id)
