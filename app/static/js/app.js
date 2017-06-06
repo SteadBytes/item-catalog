f32c3daddd90fd8edfcc76a3cc43d0b7490e158f
Jormangandur <=>
2017-06-06 09:02:28 +0100

    Style signin page, gsignin button JS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+var clicked = false;
+
+function onClick() {
+	clicked = true;
+	document.getElementById("gsignin").src = "/static/images/gsignin_pressed.png";
+}
+
+function onMouseOver() {
+	if (!clicked)
+		document.getElementById("gsignin").src = "/static/images/gsignin_focus.png";
+}
+
+function onMouseOut(obj) {
+	if (!clicked)
+		obj.src = "/static/images/gsignin.png";
+}
