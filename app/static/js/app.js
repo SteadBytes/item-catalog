57064727ef62106e3df6ec7ca8da21297f906ed4
Jormangandur <=>
2017-06-06 13:12:23 +0100

    Sign in styles

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,15 +2,15 @@ var clicked = false;
 
 function onClick() {
 	clicked = true;
-	document.getElementById("gsignin").src = "/static/images/gsignin_pressed.png";
+	document.getElementById("gsignin").src = "/static/images/gsignin_pressed.svg";
 }
 
 function onMouseOver() {
 	if (!clicked)
-		document.getElementById("gsignin").src = "/static/images/gsignin_focus.png";
+		document.getElementById("gsignin").src = "/static/images/gsignin_focus.svg";
 }
 
 function onMouseOut(obj) {
 	if (!clicked)
-		obj.src = "/static/images/gsignin.png";
+		obj.src = "/static/images/gsignin.svg";
 }
